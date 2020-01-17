d165019d66ddaa9caa4a6c911b1c622ebba0d46d
Eric(Yinghao) Li <43039401+griffithwoods@users.noreply.github.com>
2020-01-17 11:08:44 -0500

    Add files via upload
    add all files

---
+++ starter/index.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`,'utf8');
+
+//change string to json object
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req,res) => {
+    // console.log('someone access the server');
+    //load 127.0.0.1:1337, check network
+    //http://127.0.0.1:1337/yinghao return /yinghao
+    const pathName = url.parse(req.url,true).pathname; //return /hello
+    console.log(pathName);
+   
+    const id = url.parse(req.url,true).query.id;
+    console.log(id);
+    console.log(laptopData.length);
+
+    //Products overview
+    if(pathName === '/products' || pathName === '/'){
+        res.writeHead(200,{'Content-type': 'text/html'});
+
+        fs.readFile(`${__dirname}/templates/template-overview.html`,'utf-8', (err, data) => {
+            let overviewOutput = data;
+
+            fs.readFile(`${__dirname}/templates/template-card.html`,'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data,el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}',cardsOutput);
+                console.log(cardsOutput);
+                res.end(overviewOutput);
+            });
+
+        });
+        //Laptop detail
+    } else if (pathName === '/laptop' && id< laptopData.length ){
+        console.log('this is the laptop');
+        res.writeHead(200,{'Content-type': 'text/html'});
+        
+        fs.readFile(`${__dirname}/templates/template-laptop.html`,'utf-8', (err, data) => {
+
+            const laptop = laptopData[id];
+
+            const output = replaceTemplate(data,laptop);   
+            
+            res.end(output);
+        });
+
+        //IMAGES
+    } else if((/\.(jpg|jpeg|png|gif)$/i).test(pathName)){
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err,data) => {
+            res.writeHead(200, { 'Content-type': 'image/jpg'});
+            res.end(data);
+        });
+
+        // URL not found
+    }  else {
+        res.writeHead(404,{'Content-type': 'text/html'});
+        res.end('URL not found');
+    }
+
+});
+
+server.listen(1337,'127.0.0.1', () => {
+    console.log('Listening for requests now');
+});
+
+function replaceTemplate(originalHtml, laptop){
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+
+    output = output.replace(/{%ID%}/g, laptop.id);
+
+    return output;
+}
